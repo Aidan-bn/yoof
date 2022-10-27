a047b18c8b0f5603b1bba63b52c593581c549eb9
Aidan - bn <81457065+Aidan-bn@users.noreply.github.com>
2022-10-27 11:58:55 +0300

    create work section

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <section className='section-header'>
       <header className="App-header">
         <h1>
           Hello There
@@ -15,6 +16,66 @@ function App() {
         <input type='text' name='search' className='inputSearch'></input>
         <button className='btnSearch'>Search</button>
       </div>
+      </section>
+      
+
+      <section className='work'>
+        <h2>Our Service</h2>
+        
+        <div className='parent-work'>
+          <div className='child-work'>
+            <h4>Software Development</h4>
+            <div className='work-description'>
+              Hey gut will you meet nw akeuun to see them tomorro,
+              Hey gut will you meet nw akeuun to see them tomorro,
+              Hey gut will you meet nw akeuun to see them tomorro
+
+              Hey gut will you meet nw akeuun to see them tomorro,
+              Hey gut will you meet nw akeuun to see them tomorro,
+              Hey gut will you meet nw akeuun to see them tomorro
+              Hey gut will you meet nw akeuun to see them tomorro,
+              Hey gut will you meet nw akeuun to see them tomorro,
+              Hey gut will you meet nw akeuun to see them tomorro
+            </div>
+          </div>
+        </div>
+
+        <div className='parent-work'>
+          <div className='child-work'>
+            <h4>Software Development</h4>
+            <div className='work-description'>
+              Hey gut will you meet nw akeuun to see them tomorro,
+              Hey gut will you meet nw akeuun to see them tomorro,
+              Hey gut will you meet nw akeuun to see them tomorro
+              Hey gut will you meet nw akeuun to see them tomorro
+            </div>
+          </div>
+        </div>
+
+        <div className='parent-work'>
+          <div className='child-work'>
+            <h4>Software Development</h4>
+            <div className='work-description'>
+              Hey gut will you meet nw akeuun to see them tomorro,
+              Hey gut will you meet nw akeuun to see them tomorro,
+              Hey gut will you meet nw akeuun to see them tomorro
+              Hey gut will you meet nw akeuun to see them tomorro
+            </div>
+          </div>
+        </div>
+
+        <div className='parent-work'>
+          <div className='child-work'>
+            <h4>Software Development</h4>
+            <div className='work-description'>
+              Hey gut will you meet nw akeuun to see them tomorro,
+              Hey gut will you meet nw akeuun to see them tomorro,
+              Hey gut will you meet nw akeuun to see them tomorro
+              Hey gut will you meet nw akeuun to see them tomorro
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
