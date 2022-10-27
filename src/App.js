9d83833da4ee15acaa191732e73db80c6d6dbbf1
Aidan - bn <81457065+Aidan-bn@users.noreply.github.com>
2022-10-28 00:06:24 +0300

    Render header section

--- src/App.js
+++ src/App.js
@@ -8,6 +8,17 @@ function App() {
   return (
     <div className="App">
       <section className='section-header'>
+        <div className='nav-bar'>
+          <h1>LOGO</h1>
+          <ul className='nav-link' >
+            <li className='nav-list'>Home</li>
+            <li className='nav-list'>About</li>
+            <li className='nav-list'>Contact</li>
+          </ul>
+        </div>
+        <div className='humberger'>
+          <h3>humberger</h3>
+        </div>
         <header className="App-header">
           <h1>
             Hello There
@@ -15,10 +26,14 @@ function App() {
           <p className='motto'>
             # You're needs, # our working area
           </p>
+          <div className='search-desk'>
+          <input type='text' name='search' className='inputSearch-desk'></input>
+          <button className='btnSearch-desk' id='btnSearch'>Search</button>
+        </div>
         </header>
         <div className='search'>
           <input type='text' name='search' className='inputSearch'></input>
-          <button className='btnSearch'>Search</button>
+          <button className='btnSearch' id='btnSearch'>Search</button>
         </div>
       </section>
       
