679221e275650443ef5e76ead1f631076fc2376d
Aidan - bn <81457065+Aidan-bn@users.noreply.github.com>
2022-10-27 13:45:11 +0300

    Render work div

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import './App.css';
 import image from './pic/dwn.png';
+import graphic from './pic/pen.svg';
+import telecom from './pic/tower.svg';
+import codes from './pic/code.svg';
+import consult from './pic/consult.svg';
 
 function App() {
   return (
@@ -28,7 +32,8 @@ function App() {
           <div className='child-work'>
             <div className='image-work'>
               <h4>Software Development</h4>
-              <img src={image} className="illustration" />
+              {/* <img src={image} className="illustration" alt='illustration' /> */}
+              <img src={codes} className='picha' alt='graphics' />
             </div>
             <div className='work-description'>
               Hey gut will you meet nw akeuun to see them tomorro,
@@ -49,7 +54,8 @@ function App() {
           <div className='child-work'>
             <div className='image-work'>
               <h4>Telecom Services</h4>
-              <img src={image} className="illustration" />
+              {/* <img src={image} className="illustration" alt='illustration' /> */}
+              <img src={telecom} className='picha' alt='graphics' />
             </div>
             <div className='work-description'>
               Hey gut will you meet nw akeuun to see them tomorro,
@@ -64,7 +70,8 @@ function App() {
           <div className='child-work'>
             <div className='image-work'>
               <h4>Graphic & Animation</h4>
-              <img src={image} className="illustration" />
+              {/* <img src={image} className="illustration" alt='illustration' /> */}
+              <img src={graphic} className='picha' alt='graphics' />
             </div>
             <div className='work-description'>
               Hey gut will you meet nw akeuun to see them tomorro,
@@ -78,10 +85,12 @@ function App() {
         <div className='parent-work'>
           <div className='child-work'>
             <div className='image-work'>
-              <h4>Consultancy Work</h4>
-              <img src={image} className="illustration" />
+              
+              {/* <img src={image} className="illustration" alt='illustration' /> */}
+              <img src={consult} className='picha' alt='graphics' />
             </div>
             <div className='work-description'>
+            <h4>Consultancy Work</h4>
               Hey gut will you meet nw akeuun to see them tomorro,
               Hey gut will you meet nw akeuun to see them tomorro,
               Hey gut will you meet nw akeuun to see them tomorro
